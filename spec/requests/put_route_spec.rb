c38e43692d18f3b6a9ce54a37f1ae332956a8c07
Student <example@example.com>
2019-08-20 16:43:48 -0700

    add passing put destination route specs.

---
+++ spec/requests/put_route_spec.rb
@@ -0,0 +1,21 @@
+require 'rails_helper'
+
+describe "put destination route" do
+
+  it 'will update a destination' do
+    destination = FactoryBot.create(:destination)
+    put "/destinations/#{destination.id}", params: {:city => 'seattle', :country => "turkey"}
+    # binding.pry
+    expect(JSON.parse(response.body)["message"]).to eq("This destination has been updated successfully!")
+    expect(response).to have_http_status(:success)
+    updated_destination = Destination.find(destination.id)
+    expect(updated_destination.country).to eq("turkey")
+    expect(updated_destination.city).to eq("seattle")
+  end
+end
+
+# it 'returns a destination with id' do
+#   destination = FactoryBot.create(:destination)
+#   get "/destinations/#{destination.id}"
+#   expect(JSON.parse(response.body)["id"]).to eq(destination.id)
+# end
