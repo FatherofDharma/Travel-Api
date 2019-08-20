c3694726c5867a5ba3ce236082cd4fdd29deab32
Student <example@example.com>
2019-08-20 13:09:01 -0700

    add devise user model and controller

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,15 @@
+
+class UsersController < ApplicationController
+before_action :authenticate_user!
+
+def index
+  # @users = User.all
+  @user = current_user
+  render :index
+end
+
+def new
+  redirect_to new_user_registration_path
+end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
+  devise_for :users
   resources :destinations do
     resources :reviews
   end
 
+  root 'home#index'
+  # root 'devise/sessions#new'
+  # devise_scope :user do get "/" => "devise/sessions#new" end
   get '/most_reviewed_destinations/', to: 'destinations#most_reviewed'
 end
